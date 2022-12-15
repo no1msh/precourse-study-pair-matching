d90703c3aad570124e21b262db74866d73ead13d
Dev-Joco <csr8367@hanmail.net>
2022-12-15 16:03:29 +0900

    feat(model/data/mission): Level, Mission

---
+++ src/main/kotlin/pairmatching/model/data/mission/Level.kt
@@ -0,0 +1,12 @@
+package pairmatching.model.data.mission
+
+enum class Level(val value: String) {
+
+    LEVEL1("레벨1"),
+    LEVEL2("레벨2"),
+    LEVEL3("레벨3"),
+    LEVEL4("레벨4"),
+    LEVEL5("레벨5"),;
+
+    // 추가 기능 구현
+}
