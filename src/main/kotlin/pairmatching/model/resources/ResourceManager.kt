8ee805ee1c7fa6ddcf049dc814726b21d95d71e6
Dev-Joco <csr8367@hanmail.net>
2022-12-15 16:06:08 +0900

    feat(model/resources): 모든 미션 정보 리스트 생성 기능 구현

--- src/main/kotlin/pairmatching/model/resources/ResourceManager.kt
+++ src/main/kotlin/pairmatching/model/resources/ResourceManager.kt
@@ -1,5 +1,8 @@
 package pairmatching.model.resources
 
+import pairmatching.model.data.mission.Course
+import pairmatching.model.data.mission.Level
+import pairmatching.model.data.mission.Mission
 import java.io.IOException
 import java.nio.file.Paths
 import kotlin.io.path.readLines
@@ -16,4 +19,37 @@ object ResourceManager {
         }
     }
 
+    fun getAllMissions(): List<Mission> {
+        return loopCourses()
+    }
+
+    private fun loopCourses(): List<Mission> {
+        val result = mutableListOf<Mission>()
+
+        for (course in Course.values()) {
+            result.addAll(loopLevels(course))
+        }
+
+        return result
+    }
+
+    private fun loopLevels(course: Course): List<Mission> {
+        val result = mutableListOf<Mission>()
+
+        for (level in Level.values()) {
+            result.addAll(loopMissionNames(course, level))
+        }
+
+        return result
+    }
+
+    private fun loopMissionNames(course: Course, level: Level): List<Mission> {
+        val result = mutableListOf<Mission>()
+
+        for (missionName in Resource.MissionNames[level]!!) {
+            result.add(Mission(course, level, missionName))
+        }
+
+        return result
+    }
 }
