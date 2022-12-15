ea23bdbe5be954c8a1a5b70592a0ba352e070716
Dev-Joco <csr8367@hanmail.net>
2022-12-16 00:56:51 +0900

    feat(view/strings): Message.kt

---
+++ src/main/kotlin/pairmatching/view/strings/Message.kt
@@ -0,0 +1,26 @@
+package pairmatching.view.strings
+
+object Message {
+
+    const val SELECT_COMMAND = "기능을 선택하세요."
+    const val PAIR_MATCHING_RESULTS = "페어 매칭 결과입니다."
+    const val PAIR_SEPARATOR = " : "
+    const val RETRY_REMATCHING_GUIDE = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?"
+    const val INITIALIZED = "초기화 되었습니다."
+
+    object MissionTemplate {
+
+        const val SEPARATOR = " | "
+        const val LEVEL_AND_MISSIONS = "  - %s: %s"
+
+        val Template = """
+            #############################################
+            과정: %s
+            미션:
+            %s            
+            #############################################
+            과정, 레벨, 미션을 선택하세요.
+            ex) %s
+        """.trimIndent()
+    }
+}
