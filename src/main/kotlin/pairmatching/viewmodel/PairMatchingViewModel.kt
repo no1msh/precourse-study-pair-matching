f92de5a4e99ebf0a855b87e5474bfe1d83fdf9cb
Dev-Joco <csr8367@hanmail.net>
2022-12-16 01:32:20 +0900

    feat(view/viewModel): PairMatchingViewModel.kt

---
+++ src/main/kotlin/pairmatching/viewmodel/PairMatchingViewModel.kt
@@ -0,0 +1,9 @@
+package pairmatching.viewmodel
+
+import pairmatching.model.repository.PairMatchingRepository
+
+class PairMatchingViewModel(
+    private val repository: PairMatchingRepository
+) {
+    
+}
